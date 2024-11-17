269cb7c6a3a1ba790810485e36af563988e819b1
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-11-17 17:25:23 +0100

    add carousel

--- components/MostVisited.tsx
+++ components/MostVisited.tsx
@@ -1,18 +1,17 @@
 "use client";
 
-import React, { useRef } from "react";
-import { AltArrowLeftIcon, AltArrowRightIcon } from "@/lib/assets";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import { Navigation } from "swiper/modules";
+import React from "react";
 import { mostVisitedHotel } from "@/lib/data";
 import HotelCard from "./HotelCard";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 const MostVisited = () => {
-  const prev2Ref = useRef<HTMLButtonElement>(null);
-  const next2Ref = useRef<HTMLButtonElement>(null);
-
   return (
     <section className="mt-14 md:px-6 px-1">
       <h1 className="text-slate-900 dark:text-white text-2xl mb-1 font-semibold">
@@ -23,73 +22,27 @@ const MostVisited = () => {
       </p>
 
       <div className="mt-7">
-        <Swiper
-          slidesPerView={4}
-          spaceBetween={10}
-          loop={true}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
+        <Carousel
+          opts={{
+            align: "start",
           }}
-          modules={[Navigation]}
-          navigation={{
-            prevEl: prev2Ref.current,
-            nextEl: next2Ref.current,
-          }}
-          onBeforeInit={(swiper) => {
-            // @ts-ignore
-            // eslint-disable-next-line no-param-reassign
-            swiper.params.navigation.prevEl = prev2Ref.current;
-            // @ts-ignore
-            // eslint-disable-next-line no-param-reassign
-            swiper.params.navigation.nextEl = next2Ref.current;
-            swiper.navigation.update();
-          }}
-          breakpoints={{
-            0: {
-              slidesPerView: 1,
-            },
-            400: {
-              slidesPerView: 1,
-            },
-            639: {
-              slidesPerView: 2,
-            },
-            865: {
-              slidesPerView: 2,
-            },
-            1000: {
-              slidesPerView: 4,
-            },
-            1500: {
-              slidesPerView: 4,
-            },
-            1700: {
-              slidesPerView: 4,
-            },
-          }}
+          className="w-full"
         >
-          {mostVisitedHotel.map((hotel) => (
-            <SwiperSlide key={hotel.id}>
-              <HotelCard hotel={hotel} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-
-        <div className="flex items-center justify-center gap-x-2 mt-5">
-          <button
-            className="size-10 border border-slate-900/10 dark:border-white/10 cursor-pointer rounded-full flex justify-center items-center"
-            ref={prev2Ref}
-          >
-            <AltArrowLeftIcon className="size-5" />
-          </button>
-          <button
-            className="size-10 border border-slate-900/10 dark:border-white/10 cursor-pointer rounded-full flex justify-center items-center"
-            ref={next2Ref}
-          >
-            <AltArrowRightIcon className="size-5" />
-          </button>
-        </div>
+          <CarouselContent>
+            {mostVisitedHotel.map((hotel) => (
+              <CarouselItem
+                key={hotel.id}
+                className="md:basis-1/2 lg:basis-1/4"
+              >
+                <HotelCard hotel={hotel} />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <div className="flex items-center justify-center gap-x-2 mt-5">
+            <CarouselPrevious />
+            <CarouselNext />
+          </div>
+        </Carousel>
       </div>
     </section>
   );
--- components/Trending.tsx
+++ components/Trending.tsx
@@ -1,23 +1,19 @@
 "use client";
 
-import React, { useRef } from "react";
-import {
-  AltArrowLeftIcon,
-  AltArrowRightIcon,
-  ArrowRightIcon,
-} from "@/lib/assets";
+import React from "react";
+import { ArrowRightIcon } from "@/lib/assets";
 import { Button } from "./ui/button";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import { Navigation } from "swiper/modules";
 import { trendingHotel } from "@/lib/data";
 import HotelCard from "./HotelCard";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 const Trending = () => {
-  const prevRef = useRef<HTMLButtonElement>(null);
-  const nextRef = useRef<HTMLButtonElement>(null);
-
   return (
     <section className="mt-14 md:px-6 px-1">
       <h1 className="text-slate-900 dark:text-white text-2xl mb-1 font-semibold">
@@ -43,73 +39,27 @@ const Trending = () => {
       </div>
 
       <div className="mt-7">
-        <Swiper
-          slidesPerView={4}
-          spaceBetween={10}
-          loop={true}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
+        <Carousel
+          opts={{
+            align: "start",
           }}
-          modules={[Navigation]}
-          navigation={{
-            prevEl: prevRef.current,
-            nextEl: nextRef.current,
-          }}
-          onBeforeInit={(swiper) => {
-            // @ts-ignore
-            // eslint-disable-next-line no-param-reassign
-            swiper.params.navigation.prevEl = prevRef.current;
-            // @ts-ignore
-            // eslint-disable-next-line no-param-reassign
-            swiper.params.navigation.nextEl = nextRef.current;
-            swiper.navigation.update();
-          }}
-          breakpoints={{
-            0: {
-              slidesPerView: 1,
-            },
-            400: {
-              slidesPerView: 1,
-            },
-            639: {
-              slidesPerView: 2,
-            },
-            865: {
-              slidesPerView: 2,
-            },
-            1000: {
-              slidesPerView: 4,
-            },
-            1500: {
-              slidesPerView: 4,
-            },
-            1700: {
-              slidesPerView: 4,
-            },
-          }}
+          className="w-full"
         >
-          {trendingHotel.map((hotel) => (
-            <SwiperSlide key={hotel.id}>
-              <HotelCard hotel={hotel} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-
-        <div className="flex items-center justify-center gap-x-2 mt-5">
-          <button
-            className="size-10 border border-slate-900/10 dark:border-white/10 cursor-pointer rounded-full flex justify-center items-center"
-            ref={prevRef}
-          >
-            <AltArrowLeftIcon className="size-5" />
-          </button>
-          <button
-            className="size-10 border border-slate-900/10 dark:border-white/10 cursor-pointer rounded-full flex justify-center items-center"
-            ref={nextRef}
-          >
-            <AltArrowRightIcon className="size-5" />
-          </button>
-        </div>
+          <CarouselContent>
+            {trendingHotel.map((hotel) => (
+              <CarouselItem
+                key={hotel.id}
+                className="md:basis-1/2 lg:basis-1/4"
+              >
+                <HotelCard hotel={hotel} />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <div className="flex items-center justify-center gap-x-2 mt-5">
+            <CarouselPrevious />
+            <CarouselNext />
+          </div>
+        </Carousel>
       </div>
     </section>
   );
